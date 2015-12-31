49981b73aeb298c843bff69255bf6530c9222b7c
Eric Caron <eric.caron@gmail.com>
2015-12-31 10:36:03 -0600

    Initial commit of project (far from working)

---
+++ app.js
@@ -0,0 +1,40 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var nunjucks = require('nunjucks');
+var favicon = require('serve-favicon');
+var http = require('http');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+
+var env = nunjucks.configure(app.get('views'), {
+    autoescape: true,
+    express:    app 
+});
+app.set('view engine', 'nunjucks')
+app.engine('nunj', nunjucks.render);
+app.set('view engine', 'nunj');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(favicon(__dirname + '/public/images/favicon.png'));
+app.use(require('stylus').middleware(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+if (app.get('env') == 'development') {
+	app.locals.pretty = true;
+}
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});

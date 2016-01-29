8455203456fcc9331b137b1ebe3a5c0352a096d1
Eric Caron <eric.caron@gmail.com>
2016-01-29 06:58:02 -0600

    Added ability to create buttons through the Settings interface

--- controllers/settings.js
+++ controllers/settings.js
@@ -1,10 +1,32 @@
 'use strict';
 
 var express = require('express');
+var db = require('../lib/db');
 var router = express.Router();
 
 router.get('/', function (req, res) {
   res.render('settings');
+});
+
+router.post('/', function (req, res, next) {
+  if (!req.body.cmd) {
+    return next(new Error('POST without a cmd'));
+  }
+  switch (req.body.cmd) {
+    case 'add-button':
+
+      db('buttons').push(
+        { mac: req.body.mac,
+          name: req.body.name
+        }
+      );
+
+      return db.savePromise().then(function(){
+        req.flash('success', 'Button has been successfully added to your system!');
+        res.redirect('/');
+      });
+      break;
+  }
 });
 
 module.exports = router;

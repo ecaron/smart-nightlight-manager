29f80d8245a63970cd33741b4a41764069290ca4
Eric Caron <eric.caron@gmail.com>
2019-11-29 19:29:22 -0600

    We can log a new light to the database. Now to do something with it...

--- controllers/settings.js
+++ controllers/settings.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var express = require('express');
-var router = express.Router();
-var db = require('../lib/db');
+const express = require('express');
+const router = express.Router();
+const db = require('../lib/db');
 
 router.get('/', function (req, res) {
   res.render('settings');
@@ -14,15 +14,27 @@ router.post('/', function (req, res, next) {
   }
   switch (req.body.cmd) {
     case 'add-button':
-      req.flash('success', 'Default color has been successfully set for the light!');
-      light = db.get('lights').find({ id: req.body.light }).value();
-      db.get('lights').push(
-        { id: req.body.light,
-          settings: {
-            color: req.body.color
+      // req.flash('success', 'Default color has been successfully set for the light!');
+      let maxEntry = db.get('lights').sortBy('id').reverse().take(1).value()[0];
+      let newEntry = {
+        id: String(parseInt(maxEntry.id) + 1),
+        settings: {}
+      };
+      if (req.body.type === 'fastled') {
+        newEntry.type = 'fastled';
+        try {
+          let newConfig = JSON.parse(req.body.config);
+          if (!newConfig.name || !newConfig.ip) {
+            return next(new Error('POST without a cmd'));
           }
+          newEntry.settings = newConfig;
+        } catch (e) {
+          return next(e);
         }
-      ).write();
+      } else {
+        return next(new Error('Unrecognized light type'));
+      }
+      db.get('lights').push(newEntry).write();
     break;
   }
   res.redirect('/');
